fe1b8567b5f9a64caf999bce18c479e7c8dc3b49
Efrain Armando Quimbayo Caballero <equimbayo@axede.com.co>
2018-08-27 08:15:57 -0500

    Save201808270815

---
+++ ScriptJs/SamplesJs.js
@@ -0,0 +1,8 @@
+function MiFuncionS8() {
+    document.getElementById('idSample08')
+        .innerHTML = 'Este texto aparecio cuando se dio click en el boton.';
+}
+function MiFuncion10() {
+    document.getElementById('idSample10')
+        .innerHTML = 'Este texto aparecio cuando se dio click en el boton.';
+}
